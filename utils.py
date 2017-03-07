8aaa0a8992b6550bf951c13b7cfb0855735bb518
Pierre <pierrelasbleis35@gmail.com>
2017-03-07 16:38:31 +0100

    Function to read training set ready

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@
 
 
 import numpy as np
+from datetime import date
 
 """
 Function loadStoreInfo()
@@ -51,7 +52,7 @@ def loadStoreInfo(url_csv):
     else:               # If the file is not empty
 
         data = list()
-        lines = content.split('\n')[1:-1]
+        lines = content.split('\n')[1:-1] # Skip header and last (empty) line
         for l in lines:
             fields = l.split(',')
             data.append( [
@@ -68,7 +69,72 @@ def loadStoreInfo(url_csv):
                 ])
         return data
 
+        
+        
+"""
+Function loadTrainingData()
+Produce a list of entries containing the training data and the outputs. 
+
+Argument: 
+* url_csv: a string containing the location of a csv file with one header line 
+and the folowing fields: 
+    * Store: Integer
+    * DayOfWeek: Integer
+    * Date: date
+    * Sales: Integer
+    * Customers: Integer
+    * Open: Integer
+    * Promo: Integer
+    * StateHoliday: char
+    * SchoolHoliday: char
     
+Returns: 
+    * data: a list. Each element is a list containing the fields as int 
+    or char or datetime
+    * output_sales: list
+    * output_customers: list
+"""
+def loadTrainingData(url_csv):
+    
+    # Load file
+    content = -1
+    with open(url_csv, 'r') as fid:
+        content = fid.read()
+        
+    if content == -1:   # empty file or reading problem    
+        return [],[],[]
+    else:               # File read
+        data = list()
+        output_sales = list()
+        output_customers = list()
+    
+        lines = content.split('\n')[1:-1] # Skip header and last (empty) line
+        for l in lines:
+            fields = l.split(',')
+            
+            # Read fields
+            output_sales.append(int(fields[3]))
+            output_customers.append(int(fields[4]))
+            data.append([
+                int(fields[0]), # Store id
+                int(fields[1]), # Day of week
+                date(
+                    int(fields[2][:4]),     # Year 
+                    int(fields[2][5:7]),    # Month
+                    int(fields[2][8:10])),  # Day
+                int(fields[5]), # Open
+                int(fields[6]), # Promo
+                fields[7][1],   # StateHoliday
+                fields[8][1]    # SchoolHoliday
+                ])
+                
+        return data, output_sales, output_customers
 
 if __name__ == "__main__":
     print(loadStoreInfo("store.csv")[0])
+    
+    (input, output_sales, output_customers) = loadTrainingData("train.csv")
+    
+    print(input[0])
+    print("Sales: {} Customers: {}".format(
+        output_sales[0], output_customers[0]))
